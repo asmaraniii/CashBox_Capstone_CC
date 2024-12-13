df022d604d81119dc69de9f0576c44640b112c87
ery <128942912+erutm@users.noreply.github.com>
2024-12-13 08:21:51 +0700

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const { register, login, googleLogin } = require("../controllers/authController");
-const verifyToken = require("../middleware/verifyToken"); // Middleware untuk verifikasi token
+const verifyToken = require("../middleware/verifyToken");
 
 const router = express.Router();
 
-// Rute untuk register dan login
 router.post("/register", register);
 router.post("/login", login);
 router.post("/google-login", googleLogin);
 
-// Rute yang membutuhkan autentikasi
 router.get("/profile", verifyToken, (req, res) => {
   res.status(200).json({ 
     message: "Profile data", 
