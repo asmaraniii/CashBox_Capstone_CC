0288827a2d58cbf8d2c6e7b18755dc7614471f62
ery <128942912+erutm@users.noreply.github.com>
2024-12-13 08:26:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,49 +15,30 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Route untuk mengecek status server
 app.get("/", (req, res) => {
   console.log("Received request to root endpoint");
   res.status(200).json({ message: "Server is running!" });
 });  
 
-// Rute untuk autentikasi (login, register, dll)
 app.use("/api/auth", authRoute);
-
-// Route untuk user
 app.use("/api/user", userRoute);
-
-// Rute untuk pemasukan
 app.use("/api/pemasukan", pemasukanRoute);
-
-// Rute untuk pengeluaran
 app.use("/api/pengeluaran", pengeluaranRoute);
-
-// Rute untuk wallet
 app.use("/api/wallet", walletRoute);
-
-// Menambahkan rute transaksi
 app.use("/api/transaksi", transaksiRoute);
-
-// Menambahkan rute goals
 app.use("/api/goals", goalsRoute);
-
-// Menambahkan rute tabungan
 app.use("/api/tabungan", tabunganRoute);
 
-// Middleware error handling
 app.use((err, req, res, next) => {
   console.error("Error:", err.message || err);
   res.status(500).json({ message: err.message || "Something went wrong" });
 });
 
-// Menjalankan server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
