8fce265e4727103408f2031b619d504bb12fbb25
ery <128942912+erutm@users.noreply.github.com>
2024-12-13 08:23:31 +0700

    Update pengeluaranRoute.js

--- routes/pengeluaranRoute.js
+++ routes/pengeluaranRoute.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const pengeluaranController = require("../controllers/pengeluaranController"); // Pastikan path ini benar
+const pengeluaranController = require("../controllers/pengeluaranController");
 
-// Rute untuk menangani pengeluaran
-// Menambahkan pengeluaran baru
+
 router.post("/", pengeluaranController.createPengeluaran);
-
-// Mengambil semua pengeluaran
 router.get("/", pengeluaranController.getAllPengeluaran);
-
-// Mengambil pengeluaran berdasarkan ID
 router.get("/:id_pengeluaran", pengeluaranController.getPengeluaranById);
-
-// Mengupdate pengeluaran berdasarkan ID
 router.put("/:id_pengeluaran", pengeluaranController.updatePengeluaran);
-
-// Menghapus pengeluaran berdasarkan ID
 router.delete("/:id_pengeluaran", pengeluaranController.deletePengeluaran);
 
 module.exports = router;
