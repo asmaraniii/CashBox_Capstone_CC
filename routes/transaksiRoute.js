949a962955779feaed44e80ca4b2fc5b58c6b028
ery <128942912+erutm@users.noreply.github.com>
2024-12-13 08:24:27 +0700

    Update transaksiRoute.js

--- routes/transaksiRoute.js
+++ routes/transaksiRoute.js
@@ -2,22 +2,11 @@ const express = require("express");
 const router = express.Router();
 const transaksiController = require("../controllers/transaksiController");
 
-// Rute untuk mengambil semua transaksi
 router.get("/transaksi", transaksiController.getAllTransaksi);
-
-// Rute untuk mengambil semua transaksi berdasarkan bulan dan tahun
 router.get("/bulan", transaksiController.getTransaksiByBulan);
-
-// Route untuk mengambil total saldo pemasukan
 router.get("/total-pemasukan", transaksiController.getTotalPemasukan);
-
-// Route untuk mengambil total saldo pemasukan per bulan
 router.get("/total-pemasukan-bulan", transaksiController.getTotalPemasukanPerBulan);
-
-// Route untuk mengambil total saldo pengeluaran per bulan
 router.get("/total-pengeluaran-bulan", transaksiController.getTotalPengeluaranPerBulan);
-
-// Route untuk mengambil total saldo pengeluaran
 router.get("/total-pengeluaran", transaksiController.getTotalPengeluaran);
 
 module.exports = router;
