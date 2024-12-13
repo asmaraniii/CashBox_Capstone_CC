fbf0f78bfdb269ec8a5c4c25f3b99d54b65ace0e
ery <128942912+erutm@users.noreply.github.com>
2024-12-13 08:25:16 +0700

    Update walletRoute.js

--- routes/walletRoute.js
+++ routes/walletRoute.js
@@ -3,26 +3,17 @@ const { addWallet, getAllWallets, updateWalletName, getWalletById, deleteWallet 
 const validateId = require("../middleware/validateId");
 const router = express.Router();
 
-// Tambahkan wallet
 router.post("/", async (req, res, next) => {
-    // Tambahkan check untuk body
     const { uid } = req.body;
     if (!uid) {
         return res.status(400).json({ message: "UID harus disertakan dalam permintaan" });
     }
-    next();  // Lanjutkan ke controller
+    next();
 }, addWallet);
 
-// Ambil semua wallet
 router.get("/", getAllWallets);
-
-// Ambil wallet berdasarkan ID
 router.get("/:id", validateId, getWalletById);
-
-// Perbarui nama wallet
 router.put("/:id", validateId, updateWalletName);
-
-// Hapus wallet berdasarkan ID
 router.delete("/:id", validateId, deleteWallet);
 
 module.exports = router;
