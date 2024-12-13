b59ee71a2e2ad83ea40edccb1d139c851e1a95b5
ery <128942912+erutm@users.noreply.github.com>
2024-12-13 08:23:57 +0700

    Update tabunganRoute.js

--- routes/tabunganRoute.js
+++ routes/tabunganRoute.js
@@ -2,19 +2,10 @@ const express = require("express");
 const router = express.Router();
 const tabunganController = require("../controllers/tabunganController");
 
-// Tambah tabungan baru
 router.post("/", tabunganController.addTabungan);
-
-// Ambil semua tabungan berdasarkan goal
 router.get("/goal/:goal_id", tabunganController.getTabunganByGoal);
-
-// Ambil semua tabungan
 router.get("/", tabunganController.getAllTabungan);
-
-// Edit tabungan
 router.put("/:id", tabunganController.updateTabungan);
-
-// Hapus tabungan
 router.delete("/:id", tabunganController.deleteTabungan);
 
 module.exports = router;
