87a39cf4c2f625c626d6b37e350270845613baf3
ery <128942912+erutm@users.noreply.github.com>
2024-12-13 08:23:02 +0700

    Update pemasukanRoute.js

--- routes/pemasukanRoute.js
+++ routes/pemasukanRoute.js
@@ -2,19 +2,17 @@ const express = require("express");
 const router = express.Router();
 const pemasukanController = require("../controllers/pemasukanController");
 
-// Rute untuk menangani pemasukan
 router.post("/", async (req, res, next) => {
-    // Check if UID and nama_sumber_uang are present in the body
     const { uid, nama_sumber_uang } = req.body;
     if (!uid || !nama_sumber_uang) {
         return res.status(400).json({ message: "UID dan nama sumber_uang harus disertakan dalam permintaan" });
     }
-    next();  // Pass control to the next middleware
-}, pemasukanController.createPemasukan); // Menambahkan pemasukan baru
+    next(); 
+}, pemasukanController.createPemasukan);
 
-router.get("/", pemasukanController.getAllPemasukan);  // Mengambil semua pemasukan
-router.get("/:id_pemasukan", pemasukanController.getPemasukanById); // Mengambil pemasukan berdasarkan ID
-router.put("/:id_pemasukan", pemasukanController.updatePemasukan); // Mengupdate pemasukan
-router.delete("/:id_pemasukan", pemasukanController.deletePemasukan); // Menghapus pemasukan
+router.get("/", pemasukanController.getAllPemasukan); 
+router.get("/:id_pemasukan", pemasukanController.getPemasukanById);
+router.put("/:id_pemasukan", pemasukanController.updatePemasukan);
+router.delete("/:id_pemasukan", pemasukanController.deletePemasukan);
 
 module.exports = router;
